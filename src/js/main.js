68fbd7f32f749f2a8bcbcc0de5daef39540157f0
Sam Morrison <sam@sammorrison.net>
2015-11-12 23:30:16 -0500

    working on hash scrolling to the correct location on page load

--- src/js/main.js
+++ src/js/main.js
@@ -12,26 +12,36 @@
 				console.log('error');
 			});
 	});
+	
+	app.directive('endDirective', function ($location, $anchorScroll) {
+		return function(scope, element, attrs) {
+			if (scope.$last){
+				if(window.location.hash) {
+					// scroll to hash location, current method not working 100%;
+				}
+			}
+		};
+	});
 })();
 
 (function(){
 	window.onscroll = function () {
 		var comics = document.getElementsByTagName('article');
-		
+
 		// get elements that we have scrolled past but only take the most
 		// recent one we passed and set that id as the hash
 		// make sure this code doesn't affect going to a url with a hash already
-		
+
 		var hashId = "";
 		var comic;
-		
+
 		for( var i = 0, len = comics.length; i < len; i++) {
 			if(document.body.scrollTop > comics[i].offsetTop) {
 				comic = comics[i];
 				hashId = comics[i].id;
 			}
 		}
-		
+
 		// make sure this works on most devices
 		history.replaceState(null, null, "#" + hashId);
 	}
